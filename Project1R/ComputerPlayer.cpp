8c062ec2e6b5f8f5925ab0fdeda1e98ee77bac78
Jacob Gasiorowski <gasioroj@mcmaster.ca>
2024-03-27 21:58:01 -0400

    Added a bit to ComputerPlayer, not functioning yet

--- Project1R/ComputerPlayer.cpp
+++ Project1R/ComputerPlayer.cpp
@@ -1,8 +1,29 @@
 #include "ComputerPlayer.h" 
+#include "Player.h"
+#include "Game.h"
+
+void ComputerPlayer::ComputerInputEasy(Game& game) {
+
+	srand(time(0));
+
+	while (true) {
+		int row = rand() % 3;
+		int col = rand() % 3;
+		}
+}
+
 
 void ComputerPlayer::getUserName() {
 	Player::getUserName();
 	cout << "\nYou are playing with computer.";
 
 
+}
+
+bool ComputerPlayer::CheckTwoInARow() {
+
+	int count = 0;
+	int EmptyRow = -1, EmptyCol = -1;
+	return true;
+	
 }
--- Project1R/ComputerPlayer.h
+++ Project1R/ComputerPlayer.h
@@ -1,9 +1,26 @@
 #include "Player.h" 
 
 class ComputerPlayer :public Player {
+private:
+
+	int CompXPosition;
+
+	int CompYPosition;
+
+	
 
 public:
+	void ComputerInputEasy(Game& game);
+	void ComputerInputHard(Game& game);
+
+	void setCompXPosition();
+	void setCompYPosition();
+	int getCompXPosition();
+	int getCompYPosition();
 
 	void getUserName();
+	bool BlockOpponentWin();
+	bool CheckTwoInARow();
+
 
 };
